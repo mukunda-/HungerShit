9f596bdc1d48f4371e30d94b996fcba796f91cfe
mukunda- <mukunda@mukunda.com>
2014-09-01 17:08:00 -0600

    VERSION 2.0

--- src/main/java/com/mukunda/hungershit/HungerShit.java
+++ src/main/java/com/mukunda/hungershit/HungerShit.java
@@ -2,8 +2,12 @@
 
 package com.mukunda.hungershit;
 
-import org.bukkit.Bukkit;
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.bukkit.Bukkit; 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -11,37 +15,68 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
-public final class HungerShit extends JavaPlugin implements Listener{
-	@Override
+//-------------------------------------------------------------------------------------------------
+public final class HungerShit extends JavaPlugin implements Listener {
+
+	//-------------------------------------------------------------------------------------------------
+	private final HashMap<UUID,Integer> savedFoodLevel = new HashMap<>();
+	
+	//-------------------------------------------------------------------------------------------------
+	@Override 
     public void onEnable() {
 
-		getServer().getPluginManager().registerEvents( this, this );
 		getConfig().options().copyDefaults( true );
 		getConfig().addDefault( "minimum-food", 1 );
 		
 		saveConfig();
-		
+
+		getServer().getPluginManager().registerEvents( this, this );
     }
 	
+	//-------------------------------------------------------------------------------------------------
 	@EventHandler( priority = EventPriority.MONITOR )
-	public void onPlayerDie( PlayerDeathEvent ev ) {
+	public void onPlayerDie( PlayerDeathEvent event ) {
 		
-		getConfig().set( 
-			"players"+'.' + ev.getEntity().getName(), 
-			ev.getEntity().getFoodLevel() );
+		savedFoodLevel.put( 
+				event.getEntity().getUniqueId(), 
+				event.getEntity().getFoodLevel() );
 	}
 	
+	//-------------------------------------------------------------------------------------------------
 	@EventHandler( priority = EventPriority.MONITOR )
-	public void onPlayerRespawn( PlayerRespawnEvent ev ) {
-		final Player p = ev.getPlayer();
+	public void onPlayerRespawn( PlayerRespawnEvent event ) {
 		
-		Bukkit.getScheduler().scheduleSyncDelayedTask( this, new Runnable() {
-			public void run() {
-				int food = getConfig().getInt( "players"+'.'+p.getName() );
-				int minfood =getConfig().getInt( "minimum-food" ); 
-				if( food < minfood ) food = minfood;
-				p.setFoodLevel( food );
-			}
-		}, 5 );
+		final UUID id = event.getPlayer().getUniqueId();
+		final Integer food = savedFoodLevel.get(id);
+		if( food == null ) {
+			// this may happen if the server shuts down after a player
+			// dies, and then he respawns in the next server session...
+			
+			// then again when did a little null pointer exception hurt anyone
+			return;
+		}
+		new RestoreFoodTask( id, food ).runTaskLater( this, 5L );
+	
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	private class RestoreFoodTask extends BukkitRunnable {
+		private final UUID playerId;
+		private final int food;
+		
+		public RestoreFoodTask( UUID player, int food ) {
+			this.playerId = player;
+			this.food = food;
+		}
+		
+		public void run() {
+			Player player = Bukkit.getPlayer( playerId );
+			if( player == null ) {
+				// this *might* happen if the player logs out right
+				//  when he respawns to avoid his food getting taken away... 
+				return; // they escaped us...
+			}  
+			player.setFoodLevel( food );
+		}
 	}
 }
